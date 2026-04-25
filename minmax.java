df6a5481eec52fba762c8d5095c8f8ca0c2993ba
Your Name <yashshinde0558@gmail.com>
2026-04-25 22:10:24 +0530

    Add minmax class with getMinMax method and fix syntax error in sumof.java

---
+++ minmax.java
@@ -0,0 +1,32 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class minmax {
+
+    public static int getMinMax(int[] arr) {
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+            max = arr[i];
+            }
+         
+            if(arr[i]<min){
+            min = arr[i];
+            }
+             
+        }
+        return max;
+    }
+    
+
+    public static void main(String[]args) {
+        int arr[] = {1, 2, 3, 4, 5, 6};
+
+        // int result = getMinMax(arr[]);
+
+     
+        
+    }
+}
